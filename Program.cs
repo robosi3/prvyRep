efa0c9d8e978c071919c900be619f41a04f0741a
robo <robosiegl@gmail.com>
2013-12-11 10:07:51 +0100

    zm,ena

--- Program.cs
+++ Program.cs
@@ -7,9 +7,7 @@ namespace WA
     {
         /// <summary>
         /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        /// zmena
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
